1559cdf3cb70159c2010b5bbca809904d7156285
anchal2727 <aanchal@irame.ai>
2024-02-18 00:32:58 +0530

    react router project completed successfully

---
+++ 07reactRouter/src/components/User/User.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { useParams } from 'react-router-dom';
+
+
+function User() {
+    const {userid} = useParams()
+  return (
+    <div className='p-4 text-3xl text-white bg-gray-bg-red-600'>User: {userid}
+      
+    </div>
+  )
+}
+
+export default User
